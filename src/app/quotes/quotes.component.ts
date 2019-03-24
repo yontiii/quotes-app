96180b52296118d17e1bd18e6c09ee75f5ca361d
yontey <muasajohn01@gmail.com>
2019-03-24 19:50:29 +0300

    added quotes to array

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quote} from '../quote'
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+    quotes = [
+      new Quote("Don't cry because it's over, smile because it happened","Dr. Seuss","John Muasa"),
+      new Quote("Be yourself; everyone else is already taken.","Oscar Wilde","John Muasa"),
+      new  Quote("So many books, so little time","Frank Zappa","John Muasa"),
+      new Quote("You only live once, but if you do it right, once is enough.","Mae West","John Muasa")
+    ]
   constructor() { }
 
   ngOnInit() {

33545803d3f653cf58d7d520ee27af664165cd20
yontey <muasajohn01@gmail.com>
2019-03-24 23:37:17 +0300

    imported quotes class to quotes-form

--- src/app/goal-form/goal-form.component.ts
+++ src/app/goal-form/goal-form.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quote} from '../quote'
 @Component({
   selector: 'app-goal-form',
   templateUrl: './goal-form.component.html',
   styleUrls: ['./goal-form.component.css']
 })
 export class GoalFormComponent implements OnInit {
-
+  newQuote = new Quote ("","","",new Date())
   constructor() { }
 
   ngOnInit() {

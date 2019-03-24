ca8beee39f86d7d98f708e4ee92b4f8b64972b54
yontey <muasajohn01@gmail.com>
2019-03-24 22:43:55 +0300

    added date effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/date-count.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateCount'
+})
+export class DateCountPipe implements PipeTransform {
+
+  transform(value: any): number {
+    let today:Date = new Date(); //get current date and time
+    let todayWithoutTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate());
+    var dateDifference = Math.abs(todayWithoutTime-value)//returns time taken since the quote was created in milliseconds
+    const secondsInADay = 86400;
+    var dateDifferenceInSeconds = dateDifference*0.001;//converts milliseconds to seconds
+     var dateTimer = dateDifferenceInSeconds/secondsInADay;
+
+     if (dateTimer >=1 && value<todayWithoutTime){
+       return dateTimer;
+     }
+     else{
+       return 0;
+     }
+
+  }
+
+ 
+
+}

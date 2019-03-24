1a5376a7e87ec99713abbfcc65cf44b3c2f9ef9e
yontey <muasajohn01@gmail.com>
2019-03-24 22:10:05 +0300

    added date functionality

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showPublisher:boolean;
-    constructor(public quote:string, public author:string, public publisher:string){
+    constructor(public quote:string, public author:string, public publisher:string, public publishDate:Date){
         this.showPublisher=false
     }
 }

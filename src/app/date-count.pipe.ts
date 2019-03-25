10b2c43f0d1c3daafef52ffb4ad2975ddd101d96
yontey <muasajohn01@gmail.com>
2019-03-25 15:43:43 +0300

    reviewed the code for errors

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -9,6 +9,7 @@ export class DateCountPipe implements PipeTransform {
     let today:Date = new Date(); //get current date and time
     let todayWithoutTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate());
     var dateDifference = Math.abs(todayWithoutTime-value)//returns time taken since the quote was created in milliseconds
+    console.log(dateDifference);
     const secondsInADay = 86400;
     var dateDifferenceInSeconds = dateDifference*0.001;//converts milliseconds to seconds
      var dateTimer = dateDifferenceInSeconds/secondsInADay;
